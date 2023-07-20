5912fa0e3563e78813eb2cfd96ba5574e39c1513
kani81 <kaniarasiduraisamy.com>
2023-07-20 15:14:08 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=[10,15,22,46,88,30,28];
+document.write("a=[10,15,22,46,88,30,28]"+"<br>"+"<br>");
+var b=[8,1,24,60,91];
+document.write("b=[8,1,24,60,91]"+"<br>"+"<br>");
+
+for(let i=0;i<b.length;i++)
+				{
+					a.push(b[i]);
+					
+	
+				}
+				console.log(a);
